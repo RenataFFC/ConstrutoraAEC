53b5e9e15117fc8ec3335b696c0f5c59458da27f
Renata Fernandes de Franca Carneiro <renatactba@hotmail.com>
2022-11-29 17:33:07 -0300

    Versao Final AeC

---
+++ main.js
@@ -0,0 +1,17 @@
+const hamburguer = document.querySelector('.hamburguer');
+const navMenu = document.querySelector('.nav-menu');
+const links = document.querySelectorAll(".nav-item");
+
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle('active');
+    navMenu.classList.toggle('active');
+
+})
+
+for (i=0; i < links.length; i++){
+    links[i].onclick=function(){
+    navMenu.classList.remove('active');
+    hamburguer.classList.toggle('active');
+}}
+
+
